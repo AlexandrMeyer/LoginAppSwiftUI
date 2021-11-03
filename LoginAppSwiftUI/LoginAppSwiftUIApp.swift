2ff7d2517fd822f8210ca8bdaa893f1c17f61922
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-11-03 16:03:55 +0300

    Initial Commit

---
+++ LoginAppSwiftUI/LoginAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppSwiftUIApp.swift
+//  LoginAppSwiftUI
+//
+//  Created by Александр on 3.11.21.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
